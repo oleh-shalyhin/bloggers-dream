3d335e9229ac979ea26ef5666c355554f1424b08
Oleh Shalyhin <oleh.shalyhin@geniussports.com>
2023-07-09 22:12:01 +0300

    add more space between header and content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Link as RouterLink, Outlet } from 'react-router-dom';
 function App() {
   return (
     <Container maxWidth="md">
-      <Stack alignItems="center" spacing={2}>
+      <Stack alignItems="center" spacing={4}>
         <Typography variant="h2" component="h1">
           <Link component={RouterLink} to={'/'} underline="none">
             Blogger's Dream
