f81f8db6096f5f1026f64bddd474f8c546decb64
Oleh Shalyhin <oleh.shalyhin@geniussports.com>
2023-07-12 13:57:46 +0300

    load post comments from API

--- src/pages/PostDetailsPage/PostDetailsPage.tsx
+++ src/pages/PostDetailsPage/PostDetailsPage.tsx
@@ -1,10 +1,9 @@
 import { Stack } from '@mui/material';
 import { useParams } from 'react-router-dom';
-import { commentsResponseMock } from '../../mocks/mocks';
 import { CommentList, PostDetails } from '../../components';
 import { useCallback, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { fetchPostAuthor, fetchSinglePost, selectDetailedPost } from '../../store/detailedPostSlice';
+import { fetchPostAuthor, fetchPostComments, fetchSinglePost, selectDetailedPost } from '../../store/detailedPostSlice';
 import { Post } from '../../types/types';
 
 export function PostDetailsPage() {
@@ -20,6 +19,7 @@ export function PostDetailsPage() {
     try {
       const newPost: Post = await dispatch(fetchSinglePost(+postId)).unwrap();
       await dispatch(fetchPostAuthor(newPost.userId)).unwrap();
+      await dispatch(fetchPostComments(+postId)).unwrap();
     } catch (error) {
       console.error('Failed to load post');
     }
@@ -29,10 +29,10 @@ export function PostDetailsPage() {
     fetchDetailedPost();
   }, [fetchDetailedPost]);
 
-  return (
+  return post.data ? (
     <Stack spacing={4}>
-      {post.data ? <PostDetails post={post.data} /> : null}
-      <CommentList comments={commentsResponseMock.comments} />
+      <PostDetails post={post.data} />
+      <CommentList comments={post.data.comments} />
     </Stack>
-  );
+  ) : null;
 }
--- src/store/detailedPostSlice.ts
+++ src/store/detailedPostSlice.ts
@@ -1,5 +1,5 @@
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { Comment, DetailedPost, Post, RequestStatus, User } from '../types/types';
+import { Comment, DetailedPost, Post, User } from '../types/types';
 import { RootState } from './store';
 import { getFullName } from '../utils/utils';
 
@@ -23,6 +23,12 @@ export const fetchPostAuthor = createAsyncThunk('posts/fetchPostAuthor', async (
   return data;
 });
 
+export const fetchPostComments = createAsyncThunk('posts/fetchPostComments', async (postId: number) => {
+  const response = await fetch(`https://dummyjson.com/comments/post/${postId}`);
+  const data = await response.json();
+  return data.comments;
+});
+
 export const detailedPostSlice = createSlice({
   name: 'detailedPost',
   initialState,
@@ -39,6 +45,11 @@ export const detailedPostSlice = createSlice({
       .addCase(fetchPostAuthor.fulfilled, (state, action: PayloadAction<User>) => {
         if (state.data) {
           state.data.userName = getFullName(action.payload.firstName, action.payload.lastName);
+        }
+      })
+      .addCase(fetchPostComments.fulfilled, (state, action: PayloadAction<Comment[]>) => {
+        if (state.data) {
+          state.data.comments = action.payload;
         }
       });
   },
